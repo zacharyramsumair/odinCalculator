67b0f2b169e11bb3aceed1444b4e3d6762c7ae04
Zachary <ramsumairzachary@gmail.com>
2023-03-10 20:35:35 -0400

    calculator working with buttons and now with keyboard input

--- script.js
+++ script.js
@@ -3,69 +3,97 @@ let equation = document.querySelector(".equation");
 let result = document.querySelector(".displayResult");
 
 equation.textContent = "0";
+let displayExpression = "0";
 
 buttons.forEach((btn) => {
 	btn.addEventListener("click", () => updateEquation(btn));
 });
 
+let findBtn = (e) => {
+	console.log(e.keyCode);
+	let index = 0;
+
+	//go through each btn and check which one has the keyCode attribute equal to the keycode pressed
+	for (let btn of buttons) {
+		if (
+			btn.getAttribute("data-keycode") == e.keyCode ||
+			btn.getAttribute("data-keycode2") == e.keyCode
+		) {
+			// find the index of that btn
+
+			index = Array.from(buttons).indexOf(btn);
+		}
+	}
+	// send buttons[index of that button] as the parameter to update equation
+
+	updateEquation(buttons[index]);
+};
+
+document.onkeydown = findBtn;
+
 let updateEquation = (btn) => {
 	let input = btn.getAttribute("data-input");
 
 	if (input == "clear") {
-		equation.textContent = "0";
+		displayExpression = "0";
 	} else if (input == "delete") {
 		if (
-			equation.textContent.at(-1) == "+" ||
-			equation.textContent.at(-1) == "-" ||
-			equation.textContent.at(-1) == "*" ||
-			equation.textContent.at(-1) == "/"
+			displayExpression.at(-1) == "+" ||
+			displayExpression.at(-1) == "-" ||
+			displayExpression.at(-1) == "*" ||
+			displayExpression.at(-1) == "/"
 		) {
-			equation.textContent = equation.textContent.substring(
+			displayExpression = displayExpression.substring(
 				0,
-				equation.textContent.length - 2
+				displayExpression.length - 2
 			);
 		} else {
-			equation.textContent = equation.textContent.substring(
+			displayExpression = displayExpression.substring(
 				0,
-				equation.textContent.length - 1
+				displayExpression.length - 1
 			);
 		}
 
-		if (equation.textContent == "") {
-			equation.textContent = "0";
+		if (displayExpression == "") {
+			displayExpression = "0";
 		}
 	} else if (input == "=") {
 		solveEquation();
 		return;
-	} else if (equation.textContent == "0") {
-		equation.textContent = input;
+	} else if (displayExpression == "0") {
+		displayExpression = input;
 	} else if (
 		input == "+" ||
 		input == "-" ||
 		input == "*" ||
 		input == "/" ||
-		equation.textContent.at(-1) == "+" ||
-		equation.textContent.at(-1) == "-" ||
-		equation.textContent.at(-1) == "*" ||
-		equation.textContent.at(-1) == "/"
+		displayExpression.at(-1) == "+" ||
+		displayExpression.at(-1) == "-" ||
+		displayExpression.at(-1) == "*" ||
+		displayExpression.at(-1) == "/"
 	) {
-		equation.textContent += ` ${input}`;
+		displayExpression += ` ${input}`;
 	} else if (input == ".") {
-		let numbers = equation.textContent.split(" ");
+		let numbers = displayExpression.split(" ");
 		console.log(numbers);
 		if (!numbers.at(-1).includes(".")) {
-			equation.textContent += `${input}`;
+			displayExpression += `${input}`;
 		}
 	} else {
-		equation.textContent += `${input}`;
+		displayExpression += `${input}`;
 	}
+
+	console.log(displayExpression);
+	equation.textContent = displayExpression
+		.replace(/\s*\*+\s*/g, " × ")
+		.replace(/\s*\/+\s*/g, " ÷ ");
 
 	result.textContent = "";
 };
 
 let solveEquation = () => {
 	try {
-		const answer = math.evaluate(equation.textContent);
+		const answer = math.evaluate(displayExpression);
 		answer == "Infinity" ? (answer = "ERROR / 0") : answer;
 		result.textContent = answer;
 	} catch (err) {
